66951ce84e2b0a58cd6ef8dc94058dfbab121c4b
kaushal-0507 <kaushalkishore4327@gmail.com>
2026-01-19 00:54:47 +0530

    updated cookie setting

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,33 +4,23 @@ const authRouter = express.Router();
 const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 8 * 60 * 60 * 1000,
+};
+
 authRouter.post("/signup", async (req, res) => {
   try {
-    const userObj = req.body;
-    const { firstName, lastName, email, password } = userObj;
-    const allowedFields = ["firstName", "lastName", "password", "email"];
-    const isAllowed = Object.keys(userObj).every((k) =>
-      allowedFields.includes(k),
-    );
-    if (!isAllowed) {
-      throw new Error("SignUp is invalid!!");
-    }
+    const { firstName, lastName, email, password } = req.body;
 
-    const user = new User({
-      firstName,
-      lastName,
-      email,
-      password: password,
-    });
+    const user = new User({ firstName, lastName, email, password });
     const newUser = await user.save();
+
     const token = await newUser.getJwt();
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-      expires: new Date(Date.now() + 8 * 3600000),
-    });
+    res.cookie("token", token, cookieOptions);
     res.send(newUser);
   } catch (error) {
     res.status(400).send("ERROR: " + error.message);
@@ -40,25 +30,15 @@ authRouter.post("/signup", async (req, res) => {
 authRouter.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
-    if (!user) {
-      throw new Error("Invalid Credentials");
-    }
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Invalid Credentials");
 
     const isPassword = await user.validatePassword(password);
-    if (!isPassword) {
-      throw new Error("Invalid Credentials");
-    }
+    if (!isPassword) throw new Error("Invalid Credentials");
 
     const token = await user.getJwt();
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-      expires: new Date(Date.now() + 8 * 3600000),
-    });
-
+    res.cookie("token", token, cookieOptions);
     res.send(user);
   } catch (error) {
     res.status(400).send("Error: " + error.message);
@@ -67,8 +47,11 @@ authRouter.post("/login", async (req, res) => {
 
 authRouter.post("/logout", (req, res) => {
   res
-    .cookie("token", null, {
-      expires: new Date(Date.now()),
+    .cookie("token", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 0,
     })
     .send("Logout Successful!!!");
 });
